b1885bfa035e430122a11b159d070684c795cfb4
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-08-23 01:40:33 +1000

    Neuron with simple classifier

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,8 +1,90 @@
-let morph: Morph;
+
+function randomInRange(min : number, max : number) {
+  return Math.random() * (max - min + 1) + min
+}
+
+class Neuron {
+  weights : number[]
+  learningRate = 0.1
+
+  constructor(size : number) {
+    this.weights = Array.from({length: size}, () => randomInRange(-1, 1))
+  }
+
+
+  activation(input: number) {
+    return input >= 0  ? 1 : -1 ;
+  }
+
+  guess(inputs : number[] ): number {
+    let sum = inputs.map( (x, idx) => this.weights[idx] * x )
+              .reduce( (a, x) => a + x)
+    return this.activation(sum)
+  }
+
+  train(inputs : number[], target : number) {
+    let guess = this.guess(inputs);
+    let err = target - guess;
+    console.log("input", inputs, " got:", guess, "target:", target)
+    this.weights = this.weights.map( w => w * err )
+
+  }
+}
+
+type Cord = { x : number, y : number, label: number }
+
+class Points {
+  size : number
+  cords : Cord[]
+  labels : number[]
+
+  constructor(size : number) {
+    this.size = size;
+    this.randomize()
+  }
+
+  randomize() {
+    this.cords = Array.from({length: this.size}, () => {
+      let x = Math.random() * width
+      let y = Math.random() * height
+      let label = x >= y ? 1 : -1
+
+      return {x, y, label}
+    })
+
+
+  }
+
+  at(i : number) {
+    return  this.cords[i]
+  }
+
+  draw() {
+    stroke(0)
+    this.cords.forEach(c => {
+      if (c.label == 1 ) {
+        fill(255)
+      } else {
+        fill(0)
+      }
+
+      ellipse(c.x, c.y, 8, 8)
+    })
+  }
+}
+
+let points: Points;
+let neuron: Neuron;
 function setup() {
-  createCanvas(windowWidth, windowHeight)
-  morph = new Morph()
-  morph.setup()
+  createCanvas(800, 800)
+  neuron = new Neuron(2)
+
+  points = new Points(10)
+  for (let i = 0, len = 10; i < len; i++) {
+    let c = points.at(i)
+    neuron.train([c.x, c.y], c.label)
+  }
+
 }
 
 function windowResized() {
@@ -11,8 +93,11 @@ function windowResized() {
 
 function draw() {
   background(80);
-  morph.draw()
+  stroke(0)
+  line(0,0, width, height)
+  points.draw()
 }
+
 
 // // const s = ( sketch : any ) => {
 //
