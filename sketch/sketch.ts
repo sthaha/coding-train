67f274783867329822879624449c503a67ae5b28
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2019-08-25 00:09:23 +1000

    Neuron learns the weight of the Bias
    Neuron instead of receiving 2 inputs to guess and train, will now
    receive another input (Bias) which is always going to be 1.0 and the
    training will over a period of time get weight of the Bias correct.

--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -5,7 +5,6 @@ class Neuron {
   learningRate = 0.01
 
   constructor(size : number) {
-    this.size = size;
     this.weights = Array.from({length: size}, () => random(-1, 1))
   }
 
@@ -15,7 +14,7 @@ class Neuron {
 
   guess(inputs : number[] ): number {
     let sum = inputs.map( (x, idx) => this.weights[idx] * x )
-              .reduce( (a, x) => a + x)
+                    .reduce((a, x) => a + x)
     return this.activation(sum)
   }
 
@@ -30,10 +29,22 @@ class Neuron {
   }
 }
 
-function yPoint(x) {
-  return 0.75 * x + 8
+function yForX(x : number) {
+  return -0.3 * x + -0.2
 }
-type Cord = { x : number, y : number, label: number }
+
+function mapToScreen(x : number , y  : number) {
+
+  //console.log("  mapping: ", x, y)
+
+  return {
+    x : map(x, -1, 1, 0, width),
+    y : map(y, -1, 1, height, 0),
+  }
+}
+
+
+type Cord = { x : number, y : number, bias: number, label: number }
 
 class Points {
   size : number
@@ -45,8 +56,7 @@ class Points {
   }
 
   labelFor(x : number, y : number) : number {
-    //return y >= yPoint(x) ? 1 : -1
-    return y >= x ? 1 : -1
+    return y >= yForX(x) ? 1 : -1
   }
 
   randomize() {
@@ -54,7 +64,9 @@ class Points {
       let x = random(-1, 1)
       let y = random(-1, 1)
       let label = this.labelFor(x, y)
-      return {x, y, label}
+      // bias is always 1.0 and its weight needs to be learned
+      let bias = 1.0
+      return {x, y, bias, label}
     })
 
 
@@ -68,45 +80,71 @@ class Points {
     stroke(0)
     this.cords.forEach(c => {
       if (c.label == 1 ) {
-        fill(255)
+        fill(200, 220, 255)
       } else {
-        fill(0)
+        fill(100, 100, 187)
       }
 
-      let px = map(c.x, -1, 1, 0, width)
-      let py = map(c.y, -1, 1, height, 0)
-
-      ellipse(px, py, 10, 10)
+      let p = mapToScreen(c.x, c.y)
+      ellipse(p.x, p.y, 12, 12)
     })
   }
 
   markGuess(i : number, correct :  boolean) {
     if (correct) {
-      fill(0, 244, 0)
+      fill(0, 244, 122)
     } else {
       fill(244, 0, 0)
     }
 
     noStroke()
     let c = this.cords[i]
-
-    let px = map(c.x, -1, 1, 0, width)
-    let py = map(c.y, -1, 1, height, 0)
-
-    ellipse(px, py, 5, 5)
+    let p = mapToScreen(c.x, c.y)
+    ellipse(p.x, p.y, 5, 5)
   }
 }
 
-let points: Points;
+let testSet: Points;
 let neuron: Neuron;
-let size = 80
+let size = 50
 let training : Points;
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
-  neuron = new Neuron(2)
-  training = new Points(size * 2)
-  points = new Points(size)
+  training = new Points(size * 8)
+  testSet = new Points(size)
+
+  neuron = new Neuron(3)
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(30);
+  drawAxis()
+  drawLine()
+  drawGuessLine()
+
+
+  testSet.draw()
+
+  for (let i = 0; i < size; i++) {
+    let c = testSet.at(i)
+    let got = neuron.guess([c.x, c.y, c.bias])
+    let correct = got == c.label
+    testSet.markGuess(i, correct)
+    if (!correct) {
+      trainNext(10)
+      //neuron.train([c.x, c.y, c.bias], c.label)
+    }
+  }
+}
+
+function mousePressed() {
+  testSet.randomize()
 }
 
 let trainIndex = 0;
@@ -117,32 +155,47 @@ function trainNext(n : number) {
   for (let i = trainIndex; i < end; i++) {
     let c = training.at(i)
     console.log(" ... training ", i)
-    neuron.train([c.x, c.y], c.label)
+    neuron.train([c.x, c.y, c.bias], c.label)
   }
   trainIndex = end == training.size ? 0 : end
 }
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+
+function drawAxis() {
+  // | line
+  let yTop = mapToScreen(0,-1)
+  let yBottom = mapToScreen(0,1)
+
+  // --- line
+  let xLeft = mapToScreen(-1,0)
+  let xRight = mapToScreen(1,0)
+
+  stroke(120,220,140)
+  strokeWeight(1)
+  line(yTop.x, yTop.y, yBottom.x, yBottom.y)
+  line(xLeft.x, xLeft.y, xRight.x, xRight.y)
 }
 
-function draw() {
-  background(80);
-  stroke(0)
-  line(0, height, width, 0)
-  points.draw()
-
-  for (let i = 0; i < size; i++) {
-    let c = points.at(i)
-    let got = neuron.guess([c.x, c.y])
-    let correct = got == c.label
-    points.markGuess(i, got == c.label)
-    if (!correct) {
-      trainNext(15)
-      neuron.train([c.x, c.y], c.label)
-    }
-  }
+function drawAxisWithoutMapping() {
+  // easy way of drawing axis
+  stroke(100,100,180)
+  strokeWeight(2)
+  line(width/2, 0, width/2, height)
+  line(0, height/2, width, height/2)
 }
 
-function mousePressed() {
-  points.randomize()
+function drawGuessLine() {
+  //stroke(244,104,100)
+
+  //let p1 = mapToScreen(-1, yForX(-1));
+  //let p2 = mapToScreen(1, yForX(1));
+  //line(p1.x, p1.y, p2.x, p2.y)
 }
+
+function drawLine() {
+  stroke(244,204,0)
+
+  let p1 = mapToScreen(-1, yForX(-1));
+  let p2 = mapToScreen(1, yForX(1));
+  line(p1.x, p1.y, p2.x, p2.y)
+}
+
